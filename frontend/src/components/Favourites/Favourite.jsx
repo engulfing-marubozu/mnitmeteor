29d2559dbbb0d8ca001b0a2e81efe91a0ee03a9a
Rahul Raj <2019ume1141@mnit.ac.in>
2022-01-19 19:53:04 +0530

    relief

---
+++ frontend/src/components/Favourites/Favourite.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Typography } from "@mui/material";
+import ArrowBackOutlinedIcon from "@mui/icons-material/ArrowBackOutlined";
+function Favourites() {
+  return (
+    <div>
+      <ArrowBackOutlinedIcon />
+      <Typography>My Favourites</Typography>
+    </div>
+  );
+}
+
+export default Favourites;

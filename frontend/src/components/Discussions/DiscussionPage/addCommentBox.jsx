475ef0882dfc307080c6f457e025fd75051d3b8c
Rahul Raj <2019ume1141@mnit.ac.in>
2022-03-12 23:38:08 +0530

    bleh

--- frontend/src/components/Discussions/DiscussionPage/addCommentBox.jsx
+++ frontend/src/components/Discussions/DiscussionPage/addCommentBox.jsx
@@ -3,6 +3,7 @@ import { TextField, Box } from '@mui/material';
 import { CommentButton } from '../DiscussionStyling/discussionStyling';
 import { createTheme } from '@mui/material/styles';
 import { ThemeProvider } from '@emotion/react';
+import {useSelector} from 'react-redux'
 const theme = createTheme({
     palette: {
         primary: {
@@ -50,9 +51,19 @@ function AddCommentBox({ addCommentData }) {
         setDisabledPost(true);
         //    setFocused(false);
     }
-    const submitHandler = () => {
+    const submitHandler =async  () => {
         console.log(inputComment.current.value);
         console.log(addCommentData);
+        const response = await axios.post(
+            "http://localhost:5000/add_comment",
+            { thread_id: cardId, comment_id: null , commentor_mnit_id: , content:inputComment.current.value},
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+        );
+        console.log(response.data);
     }
     return (
         <ThemeProvider theme={theme}>

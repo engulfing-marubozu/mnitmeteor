6c88601ec13b195daa0e95203d7854dff4a0a64f
Rahul Raj <2019ume1141@mnit.ac.in>
2022-04-29 00:17:55 +0530

    apli call limiter uncommented

--- backend/routes/interested.js
+++ backend/routes/interested.js
@@ -2,13 +2,13 @@ const express = require("express");
 const { send_interested_products, interested_update }= require("../controllers/interested/interested");
 const { un_interested_update}= require("../controllers/interested/un_interested");
 const {authorization} = require("../Middlewares/authorization")
-//const {api_call_limiter} = require("../Middlewares/api_call_limiter")
+const {api_call_limiter} = require("../Middlewares/api_call_limiter")
 const router = express.Router();
 
 
 router.get('/send_interested_products',authorization, send_interested_products);
 router.post('/interested_update',authorization, interested_update);
-router.post('/un_interested_update',  authorization, un_interested_update);
+router.post('/un_interested_update',  authorization,api_call_limiter, un_interested_update);
 
 module.exports= router;
 

4087ba898252f918de0be12878f9852af0888046
Rahul Raj <2019ume1141@mnit.ac.in>
2022-05-05 00:43:21 +0530

    bleh

--- frontend/src/components/HomePage/productCategory/electronics.jsx
+++ frontend/src/components/HomePage/productCategory/electronics.jsx
@@ -22,7 +22,7 @@ function Electronics() {
     (node) => {
       if (loading) return;
       if (observer.current) observer.current.disconnect();
-      observer.current = new IntersectionObserver((entries) => {S
+      observer.current = new IntersectionObserver((entries) => {
         if (entries[0].isIntersecting && hasMore) {
           setPointer((prev) => prev + 20);
         }
